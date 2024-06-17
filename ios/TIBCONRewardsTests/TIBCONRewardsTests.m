38fc8dd0355ec6c27bb1561a12eb4553eba65816
Nishank Phulera <nishank.phulera@genuinemark.org>
2024-06-17 22:52:15 +0530

    Renamed to TIBCON Rewards

--- ios/TIBCONRewardsTests/TIBCONRewardsTests.m
+++ ios/TIBCONRewardsTests/TIBCONRewardsTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface TibconTests : XCTestCase
+@interface TIBCONRewardsTests : XCTestCase
 
 @end
 
-@implementation TibconTests
+@implementation TIBCONRewardsTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
